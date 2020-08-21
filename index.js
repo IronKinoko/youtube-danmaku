f17749a2c37efe24ddefe5b1471a8e450d91ab05
IronKinoko <kinoko_main@outlook.com>
2020-08-22 03:31:06 +0800

    fix: 修复出错 基本能用了

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function init() {
     document.getElementById('ytd-player').classList.add('container')
     window.CM = new CommentManager(document.querySelector('#ytd-player'))
     CM.options.global.scale = 1.5
-    CM.options.global.opacity = 0.6
+    CM.options.global.opacity = 0.7
     CM.init() // 初始化
     CM.start()
     timeKey = setInterval(() => {
@@ -32,23 +32,24 @@ function init() {
 }
 
 function getDanmaku() {
-  /**
-   * @type {Document}
-   */
-  const idoc = document.querySelector('iframe#chatframe').contentDocument
-  const messagesNode = Array.from(
-    idoc.querySelectorAll('yt-live-chat-text-message-renderer'),
-  )
-
-  const lastMessageNode = messagesNode.pop()
-  if (lastMessageNode) {
-    const nextID = lastMessageNode.id
-    const message = lastMessageNode.querySelector('#message').innerHTML
-
-    playing &&
-      prevID !== nextID &&
-      CM.send({ text: message, mode: 1, color: 0xffffff })
-    prevID = nextID
+  const iframe = document.querySelector('iframe#chatframe')
+  if (iframe) {
+    /**
+     * @type {Document}
+     */
+    const idoc = iframe.contentDocument
+    const messagesNode = Array.from(
+      idoc.querySelectorAll('yt-live-chat-text-message-renderer'),
+    )
+    const lastMessageNode = messagesNode.pop()
+    if (lastMessageNode) {
+      const nextID = lastMessageNode.id
+      const message = lastMessageNode.querySelector('#message').textContent
+      playing &&
+        prevID !== nextID &&
+        CM.send({ text: message, mode: 1, color: 0xffffff })
+      prevID = nextID
+    }
   }
 }
 
@@ -80,14 +81,18 @@ function buildControls() {
 function subEvent() {
   const video = document.querySelector('video')
   video.addEventListener('pause', () => {
-    CM.stop()
     playing = false
+    CM.stop()
     button.textContent = '弹幕: 关'
+    clearInterval(timeKey)
   })
   video.addEventListener('play', () => {
+    playing = true
     CM.clear()
     CM.start()
-    playing = true
     button.textContent = '弹幕: 开'
+    timeKey = setInterval(() => {
+      getDanmaku()
+    }, 100)
   })
 }
