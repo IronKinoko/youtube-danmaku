e07b9c8f4bf6e98cab888cdc0b2c2de4b962a30e
IronKinoko <kinoko_main@outlook.com>
2020-08-23 05:46:39 +0800

    feat: 更新控制条，采用react搭建应用

---
+++ CM.js
@@ -0,0 +1,112 @@
+let playing = true
+let timeKey
+let prevID
+let CM
+function init() {
+  try {
+    document.getElementById('player-container').classList.add('abp')
+    document.getElementById('ytd-player').classList.add('container')
+    document
+      .querySelector('div.ytp-left-controls')
+      .setAttribute('style', 'overflow: unset;')
+    CM = new CommentManager(document.querySelector('#ytd-player'))
+    CM.init() // 初始化
+
+    buildControls()
+    subEvent()
+  } catch (e) {
+    console.error(e)
+    setTimeout(() => {
+      init()
+    }, 3000)
+  }
+}
+
+function getDanmaku() {
+  const iframe = document.querySelector('iframe#chatframe')
+  if (iframe) {
+    /**
+     * @type {Document}
+     */
+    const idoc = iframe.contentDocument
+    const messagesNode = Array.from(
+      idoc.querySelectorAll('yt-live-chat-text-message-renderer')
+    )
+    const lastMessageNode = messagesNode.pop()
+    if (lastMessageNode) {
+      const nextID = lastMessageNode.id
+      const message = lastMessageNode.querySelector('#message').textContent
+      playing &&
+        prevID !== nextID &&
+        CM.send({ text: message, mode: 1, color: 0xffffff })
+      prevID = nextID
+    }
+  }
+}
+
+function buildControls() {
+  const div = document.createElement('div')
+  div.style.width = 'auto'
+  div.id = 'ytb-danmaku-config'
+  document.querySelector('.ytp-left-controls').append(div)
+}
+
+function subEvent() {
+  const video = document.querySelector('video')
+  video.addEventListener('pause', () => {
+    playing = false
+    CM.stop()
+    clearInterval(timeKey)
+  })
+  video.addEventListener('play', () => {
+    playing = true
+    CM.clear()
+    CM.start()
+    timeKey = setInterval(() => {
+      getDanmaku()
+    }, 100)
+  })
+
+  window.addEventListener('resize', () => {
+    CM.init(document.querySelector('#ytd-player'))
+  })
+
+  video.addEventListener('fullscreenchange', () => {
+    CM.init(document.querySelector('#ytd-player'))
+  })
+}
+
+/**
+ * @param {boolean} use
+ */
+function toggleDanmaku(use) {
+  if (use) {
+    playing = true
+    CM.clear()
+    CM.start()
+    timeKey = setInterval(() => {
+      getDanmaku()
+    }, 100)
+  } else {
+    playing = false
+    CM.stop()
+    CM.clear()
+    clearInterval(timeKey)
+  }
+}
+
+/**
+ * @param {number} scale
+ */
+function changeDanmakuSpeed(scale) {
+  CM.options.global.scale = 2.1 - scale
+}
+
+/**
+ * @param {number} opacity
+ */
+function changeOpacity(opacity) {
+  CM.options.global.opacity = opacity
+}
+
+export { init, toggleDanmaku, changeDanmakuSpeed, changeOpacity }
---
+++ Danmaku.js
@@ -0,0 +1,184 @@
+import React, { useState, useEffect } from 'react'
+import Tooltip from '@material-ui/core/Tooltip'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
+import Switch from '@material-ui/core/Switch'
+import ClickAwayListener from '@material-ui/core/ClickAwayListener'
+import Box from '@material-ui/core/Box'
+import Fade from '@material-ui/core/Fade'
+import makeStyles from '@material-ui/core/styles/makeStyles'
+import createStyles from '@material-ui/core/styles/createStyles'
+import Slider from '@material-ui/core/Slider'
+import { changeDanmakuSpeed, changeOpacity, toggleDanmaku } from './CM'
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    root: {
+      position: 'relative',
+    },
+    controls: {
+      backgroundColor: 'rgba(28,28,28,0.9)',
+      position: 'absolute',
+      bottom: 40,
+      color: 'white',
+      left: '50%',
+      transform: 'translateX(-50%)',
+      zIndex: theme.zIndex.tooltip,
+      width: 300,
+    },
+    sliderRoot: {
+      display: 'flex',
+      alignItems: 'center',
+    },
+    slider: {
+      margin: theme.spacing(0, 1, 0, 2),
+      flex: 1,
+    },
+    tooltip: {
+      padding: theme.spacing(0.5, 1),
+      backgroundColor: 'rgba(28,28,28,0.9)',
+      fontSize: 13,
+      borderRadius: 2,
+      fontWeight: 400,
+    },
+    listButton: {
+      '&:hover': {
+        backgroundColor: 'rgba(255,255,255,.1)',
+      },
+    },
+  })
+)
+const Danmaku = () => {
+  const [open, setOpen] = useState(false)
+  const classes = useStyles()
+  const [config, setConfig] = useLocalStorageState('ytb-danmaku-config', {
+    scale: 1.5,
+    opacity: 0.7,
+    use: false,
+  })
+
+  useEffect(() => {
+    toggleDanmaku(config.use)
+    changeDanmakuSpeed(config.scale)
+    changeOpacity(config.opacity)
+  }, [])
+
+  const handleSwitch = () => {
+    setConfig((t) => {
+      toggleDanmaku(!t.use)
+      return { ...t, use: !t.use }
+    })
+  }
+
+  return (
+    <span className={classes.root}>
+      <Tooltip
+        title="弹幕"
+        placement="top"
+        classes={{ tooltip: classes.tooltip }}
+      >
+        <button
+          style={{ textAlign: 'center' }}
+          onClick={() => setOpen(true)}
+          className="ytp-button"
+        >
+          弹幕
+        </button>
+      </Tooltip>
+
+      <Fade in={open} unmountOnExit>
+        <Box className={classes.controls}>
+          <ClickAwayListener onClickAway={() => setOpen(false)}>
+            <List>
+              <ListItem
+                button
+                className={classes.listButton}
+                onClick={handleSwitch}
+              >
+                <ListItemText
+                  primary="弹幕开关"
+                  primaryTypographyProps={{
+                    className: 'ytp-menuitem-label',
+                    style: { fontWeight: 500 },
+                  }}
+                />
+                <ListItemSecondaryAction>
+                  <Switch checked={config.use} onClick={handleSwitch} />
+                </ListItemSecondaryAction>
+              </ListItem>
+              <ListItem className={classes.listButton}>
+                <ListItemText
+                  primary={
+                    <div className={classes.sliderRoot}>
+                      <div className="ytp-menuitem-label">弹幕速度</div>
+                      <Slider
+                        color="secondary"
+                        max={2}
+                        step={0.1}
+                        min={0.1}
+                        value={config.scale}
+                        valueLabelDisplay="auto"
+                        className={classes.slider}
+                        onChange={(e, v) => {
+                          setConfig((t) => ({ ...t, scale: v }))
+                          changeDanmakuSpeed(v)
+                        }}
+                      />
+                    </div>
+                  }
+                />
+              </ListItem>
+              <ListItem className={classes.listButton}>
+                <ListItemText
+                  primary={
+                    <div className={classes.sliderRoot}>
+                      <div className="ytp-menuitem-label">不透明度</div>
+                      <Slider
+                        color="secondary"
+                        max={1}
+                        step={0.1}
+                        min={0}
+                        value={config.opacity}
+                        valueLabelDisplay="auto"
+                        className={classes.slider}
+                        onChange={(e, v) => {
+                          setConfig((t) => ({ ...t, opacity: v }))
+                          changeOpacity(v)
+                        }}
+                      />
+                    </div>
+                  }
+                />
+              </ListItem>
+            </List>
+          </ClickAwayListener>
+        </Box>
+      </Fade>
+    </span>
+  )
+}
+
+export default Danmaku
+
+/**
+ * @param {string} key
+ * @param {any} defaultValue
+ */
+function useLocalStorageState(key, defaultValue) {
+  const [value, setValue] = useState(() => {
+    let res = localStorage.getItem(key)
+    if (res !== null) {
+      try {
+        return JSON.parse(res)
+      } catch (error) {}
+    }
+    return defaultValue
+  })
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value))
+  }, [value, key])
+
+  return [value, setValue]
+}
