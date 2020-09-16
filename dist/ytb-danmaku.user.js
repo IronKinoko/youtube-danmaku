f4e192a48e95f337fb0b5e395aa8208eeaacfe61
IronKinoko <kinoko_main@outlook.com>
2020-09-16 23:08:33 +0800

    2.1.0 build core

--- dist/ytb-danmaku.user.js
+++ dist/ytb-danmaku.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         youtube-danmaku
 // @namespace    https://github.com/IronKinoko/ytb-danmaku
-// @version      2.0.19
+// @version      2.1.0
 // @license      MIT
 // @description  Youtube livechat danmaku
 // @author       Ironkinoko
@@ -9,7 +9,7 @@
 // @exclude      https://www.youtube.com/live_chat*
 // @grant        none
 // @require      https://cdn.jsdelivr.net/npm/comment-core-library-html-comment@1.0.0/dist/CommentCoreLibrary.js
-// @require      https://cdn.jsdelivr.net/gh/IronKinoko/ytb-danmaku@9802b27a63bb4ee629efaadcc488db3fe930e880/dist/ytb-danmaku-core.min.js
+// @require      https://cdn.jsdelivr.net/gh/IronKinoko/ytb-danmaku@##hash##/dist/ytb-danmaku-core.min.js
 // ==/UserScript==
 
 ;(function () {
