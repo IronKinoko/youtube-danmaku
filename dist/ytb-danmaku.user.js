0f1b101a998dd5ad66b5a8504bcdd2d1463cf4b3
IronKinoko <kinoko_main@outlook.com>
2020-11-15 19:17:04 +0800

    2.1.4 release

--- dist/ytb-danmaku.user.js
+++ dist/ytb-danmaku.user.js
@@ -9,7 +9,7 @@
 // @exclude      https://www.youtube.com/live_chat*
 // @grant        none
 // @require      https://cdn.jsdelivr.net/npm/comment-core-library-html-comment@1.0.0/dist/CommentCoreLibrary.js
-// @require      https://cdn.jsdelivr.net/gh/IronKinoko/ytb-danmaku@##hash##/dist/ytb-danmaku-core.min.js
+// @require      https://cdn.jsdelivr.net/gh/IronKinoko/ytb-danmaku@44a4a69eccdbf00cf3827ea25db1cdbb9cc17f38/dist/ytb-danmaku-core.min.js
 // ==/UserScript==
 
 ;(function () {
