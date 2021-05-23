1ed63395390347d6f68628e5613d90af92f9acac
IronKinoko <IronKinoko@users.noreply.github.com>
2021-05-23 13:33:38 +0000

    Deploying to gh-pages from @ IronKinoko/ytb-danmaku@d98727918fb7d2d465f284337c616b906e92f077 🚀

--- index.user.js
+++ index.user.js
@@ -1,15 +1,15 @@
 // ==UserScript==
 // @name         youtube-danmaku
 // @namespace    https://github.com/IronKinoko/ytb-danmaku
-// @version      2.4.2
+// @version      2.4.3
 // @license      MIT
 // @description  Youtube livechat danmaku
 // @author       Ironkinoko
 // @match        https://www.youtube.com/*
 // @exclude      https://www.youtube.com/live_chat*
 // @grant        none
-// @require      https://cdn.jsdelivr.net/npm/react@17.0.2/umd/react.production.min.js
-// @require      https://cdn.jsdelivr.net/npm/react-dom@17.0.2/umd/react-dom.production.min.js
+// @require      https://cdn.jsdelivr.net/npm/react@16.14.0/umd/react.production.min.js
+// @require      https://cdn.jsdelivr.net/npm/react-dom@16.14.0/umd/react-dom.production.min.js
 // @require      https://cdn.jsdelivr.net/npm/mobx@6.3.2/dist/mobx.umd.production.min.js
 // @require      https://cdn.jsdelivr.net/npm/mobx-react-lite@3.2.0/dist/mobxreactlite.umd.production.min.js
 // @require      https://cdn.jsdelivr.net/npm/mobx-react@7.2.0/dist/mobxreact.umd.production.min.js
